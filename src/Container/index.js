acbada1c4230d8d014df17f78278bc85f93ea2bb
Filip Buchwald <fixu1998@gmail.com>
2022-11-15 12:59:47 +0100

    created styled container

---
+++ src/Container/index.js
@@ -0,0 +1,9 @@
+import { Main } from "./styled";
+
+const Container = (props) => (
+  <Main>
+    {props.children}
+  </Main>
+);
+
+export default Container
---
+++ src/Container/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+  align-items: center;
+`;
